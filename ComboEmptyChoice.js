841eaccf37c578df320816474f60ae4210827bcd
Marcio <marcio.cruz@ispm.com.br>
2012-04-16 18:14:37 -0300

    missing files

---
+++ ComboEmptyChoice.js
@@ -0,0 +1,43 @@
+Ext.define('Ext.ux.plugin.ComboEmptyChoice', {
+    extend: 'Ext.AbstractPlugin',
+    
+    init: function(combo){
+        var me = this;
+        combo.clearOption = combo.clearOption || '<i>Clear</i>';
+        combo.setValue = this._setValue;
+        
+        if(combo.getStore()){
+            combo.getStore().on('load', function(s, r, ok, op, eop){
+                me.storeLoad(s, r, ok, op, eop, combo);
+            });
+        }
+    },
+    
+    /*
+     * Creates a dummy instance of the store's model to act as the blank option.
+     */
+    storeLoad: function(s, r, ok, op, eop, combo){
+        var config = {};
+        config[combo.displayField] = combo.clearOption;
+        config[combo.valueField] = -1;
+        
+        var model = Ext.create(s.model, config);
+        s.add(model);
+    },
+
+    /*
+     * If the choosen option was the blank one, clears the combobox.
+     */
+    _setValue: function(value){
+        if(!value){
+            return;
+        }
+        if(!(value instanceof Array)){
+            value = [value];
+        }
+        if(value[0].data[this.valueField] == -1){
+            return Ext.form.field.ComboBox.prototype.setValue.apply(this, []);
+        }
+        return Ext.form.field.ComboBox.prototype.setValue.apply(this, value);
+    }
+});
